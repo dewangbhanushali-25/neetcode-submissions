f1e346675c892f84d8d508c4d274a34aa19b6c1f
dewang <140656832+dewangbhanushali-25@users.noreply.github.com>
2026-07-12 02:53:05 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length()!= t.length())
+            return false;
+
+        int freq[128] ={0};
+         
+
+        for (char ch :s)
+            freq[ch]++;
+
+        for (char ch :t)
+            freq[ch]--;
+
+        for (int i = 0 ; i < 128 ;i++){
+            if (freq[i]!= 0)
+                return false;
+
+        }        
+
+        return true;
+
+        
+    }
+};
